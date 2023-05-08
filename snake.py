72a00c9a440128354d46271ab4389af115aed4e6
dead-insult <124542304+dead-insult@users.noreply.github.com>
2023-05-08 15:33:52 +1000

    Update snake.py

--- snake.py
+++ snake.py
@@ -3,6 +3,7 @@ import time
 import random
 
 Game_Running = True
+window_running = True
 
 game_width = 500
 game_height = 500
@@ -10,23 +11,22 @@ snake_item = 20
 snake_color1 = "red"
 snake_color2 = "yellow"
 
-virtual_game_x = game_width//snake_item
-virtual_game_y = game_height//snake_item
+virtual_game_x = game_width // snake_item
+virtual_game_y = game_height // snake_item
 
-snake_x=virtual_game_x//2
-snake_y=virtual_game_y//2
+snake_x = virtual_game_x // 2
+snake_y = virtual_game_y // 2
 snake_x_nav = 0
 snake_y_nav = 0
 
 snake_list = []
 snake_size = 5
-
-
+sneak_score = 0
 
 tk = Tk()
 tk.title("Игра Змейка на Python")
-tk.resizable(0,0)
-tk.wm_attributes("-topmost",1)
+tk.resizable(0, 0)
+tk.wm_attributes("-topmost", 1)
 canvas = Canvas(tk, width=game_width, height=game_height, bd=0, highlightthickness=0)
 canvas.pack()
 tk.update()
@@ -38,43 +38,48 @@ presents_size = 25
 for i in range(presents_size):
     x = random.randrange(virtual_game_x)
     y = random.randrange(virtual_game_y)
-    id1 = canvas.create_oval(x*snake_item,y*snake_item, x*snake_item+snake_item,y*snake_item+snake_item,fill=present_color2)
-    id2 = canvas.create_oval(x*snake_item+2,y*snake_item+2,x*snake_item+snake_item-2,y*snake_item+snake_item-2,fill=present_color1)    
+    id1 = canvas.create_oval(x * snake_item, y * snake_item, x * snake_item + snake_item, y * snake_item + snake_item,
+                             fill=present_color2)
+    id2 = canvas.create_oval(x * snake_item + 2, y * snake_item + 2, x * snake_item + snake_item - 2,
+                             y * snake_item + snake_item - 2, fill=present_color1)
     presents_list.append([x, y, id1, id2])
 print(presents_list)
 
+
 def snake_paint_item(canvas, x, y):
     global snake_list
-    id1 = canvas.create_rectangle(x*snake_item,y*snake_item, x*snake_item+snake_item,y*snake_item+snake_item,fill=snake_color2)
-    id2 = canvas.create_rectangle(x*snake_item+2,y*snake_item+2,x*snake_item+snake_item-2,y*snake_item+snake_item-2,fill=snake_color1)
-    snake_list.append([x,y,id1,id2])
-    #print(snake_list)
+    id1 = canvas.create_rectangle(x * snake_item, y * snake_item, x * snake_item + snake_item,
+                                  y * snake_item + snake_item, fill=snake_color2)
+    id2 = canvas.create_rectangle(x * snake_item + 2, y * snake_item + 2, x * snake_item + snake_item - 2,
+                                  y * snake_item + snake_item - 2, fill=snake_color1)
+    snake_list.append([x, y, id1, id2])
+
 
 snake_paint_item(canvas, snake_x, snake_y)
+
 
 def check_can_we_delete_snake_item():
     if len(snake_list) >= snake_size:
         temp_item = snake_list.pop(0)
-        #print(temp_item)
         canvas.delete(temp_item[2])
         canvas.delete(temp_item[3])
+
 
 def check_if_we_found_present():
     global snake_size
     for i in range(len(presents_list)):
         if presents_list[i][0] == snake_x and presents_list[i][1] == snake_y:
-            #print("found!!!")
             snake_size = snake_size + 1
             canvas.delete(presents_list[i][2])
             canvas.delete(presents_list[i][3])
-    #print(snake_x, snake_y)
+
 
 def snake_move(event):
     global snake_x
     global snake_y
     global snake_x_nav
     global snake_y_nav
-    
+
     if event.keysym == "Up":
         snake_x_nav = 0
         snake_y_nav = -1
@@ -96,26 +101,68 @@ def snake_move(event):
     snake_paint_item(canvas, snake_x, snake_y)
     check_if_we_found_present()
 
+
 canvas.bind_all("<KeyPress-Left>", snake_move)
 canvas.bind_all("<KeyPress-Right>", snake_move)
 canvas.bind_all("<KeyPress-Up>", snake_move)
 canvas.bind_all("<KeyPress-Down>", snake_move)
 
+
 def game_over():
     global Game_Running
     Game_Running = False
 
+
 def check_if_borders():
-    if snake_x>virtual_game_x or snake_x<0 or snake_y>virtual_game_y or snake_y<0:
+    if snake_x > virtual_game_x or snake_x < 0 or snake_y > virtual_game_y or snake_y < 0:
+        canvas.create_text(game_width / 2, game_height / 2,
+                           text="GAME OVER!",
+                           font="Arial 20",
+                           fill="#ff0000")
+        canvas.create_text(250, 350,
+                           text="For restart press R",
+                           font="Arial 15",
+                           fill="black")
+        canvas.create_text(250, 390,
+                           text="For exit press E",
+                           font="Arial 15",
+                           fill="black")
         game_over()
+
 
 def check_we_touch_self(f_x, f_y):
     global Game_Running
     if not (snake_x_nav == 0 and snake_y_nav == 0):
         for i in range(len(snake_list)):
             if snake_list[i][0] == f_x and snake_list[i][1] == f_y:
+                canvas.create_text(game_width / 2, game_height / 2,
+                                   text="GAME OVER!",
+                                   font="Arial 20",
+                                   fill="#ff0000")
+                canvas.create_text(250, 350,
+                                   text="For restart press R",
+                                   font="Arial 15",
+                                   fill="black")
+                canvas.create_text(250, 390,
+                                   text="For exit press E",
+                                   font="Arial 15",
+                                   fill="black")
                 print("found!!!")
+
                 Game_Running = False
+
+
+def score():
+    global snake_size, sneak_score, text
+    for i in range(len(presents_list)):
+        if presents_list[i][0] == snake_x and presents_list[i][1] == snake_y:
+            sneak_score += 1
+            canvas.delete(text)
+            text = canvas.create_text(50, 50, text=sneak_score, font="Arial 20", fill="black")
+
+
+text = canvas.create_text(50, 50, text=sneak_score, font="Arial 20", fill="black")
+
 while Game_Running:
     check_can_we_delete_snake_item()
     check_if_we_found_present()
@@ -127,10 +174,4 @@ while Game_Running:
     tk.update_idletasks()
     tk.update()
     time.sleep(0.15)
-
-def fun_nothing(event):
-    pass
-canvas.bind_all("<KeyPress-Left>", fun_nothing)
-canvas.bind_all("<KeyPress-Right>", fun_nothing)
-canvas.bind_all("<KeyPress-Up>", fun_nothing)
-canvas.bind_all("<KeyPress-Down>", fun_nothing)
+    score()
