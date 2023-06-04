81b3c97c6ccabdf135327d3ba811a535ebbc1dd6
dead-insult <124542304+dead-insult@users.noreply.github.com>
2023-06-04 21:38:26 +1000

    last Update snake.py

--- snake.py
+++ snake.py
@@ -1,196 +1,172 @@
 from tkinter import *
-import time
 import random
 
-Game_Running = True
-text = ""
+# глобальные переменные
+CANVAS_WIDTH = 640
+CANVAS_HEIGHT = 480
+SNAKE_SIZE = 20
+MOVE_INCREMENT = 20
+DELAY = 100  # задержка перед следующим ходом, в миллисекундах
 
-game_width = 500
-game_height = 500
-snake_item = 20
-snake_color1 = "red"
-snake_color2 = "yellow"
-speed_sneak = 0.15
+# цвета
+SNAKE_COLOR = 'green'
+FOOD_COLOR = 'red'
+BONUS_COLOR = 'yellow'
+WALL_COLOR = 'gray'
+BACKGROUND_COLOR = 'black'
+TEXT_COLOR = 'white'
 
+# пересчёт координат
+def get_new_position(coords, direction):
+    x, y = coords[-1]  # координаты головы змеи
+    if direction == 'Up':
+        return x, y - SNAKE_SIZE
+    elif direction == 'Down':
+        return x, y + SNAKE_SIZE
+    elif direction == 'Left':
+        return x - SNAKE_SIZE, y
+    elif direction == 'Right':
+        return x + SNAKE_SIZE, y
 
-virtual_game_x = game_width // snake_item
-virtual_game_y = game_height // snake_item
+# генерация случайной позиции
+def get_random_position():
+    x = random.randrange(0, CANVAS_WIDTH - SNAKE_SIZE, SNAKE_SIZE)
+    y = random.randrange(0, CANVAS_HEIGHT - SNAKE_SIZE, SNAKE_SIZE)
+    return x, y
 
-snake_x = virtual_game_x // 2
-snake_y = virtual_game_y // 2
-snake_x_nav = 0
-snake_y_nav = 0
+# рисование змеи
+def draw_snake(canvas, coords):
+    for i, (x, y) in enumerate(coords):
+        if i == len(coords) - 1:  # голова змеи
+            color = SNAKE_COLOR
+        else:
+            color = WALL_COLOR
+        canvas.create_rectangle(x, y, x + SNAKE_SIZE, y + SNAKE_SIZE, fill=color)
 
-snake_list = []
-snake_size = 5
-sneak_score = 0
+# рисование еды
+def draw_food(canvas, food_pos):
+    x, y = food_pos
+    canvas.create_oval(x, y, x + SNAKE_SIZE, y + SNAKE_SIZE, fill=FOOD_COLOR)
 
-present_color1 = "blue"
-present_color2 = "black"
-presents_list = []
-presents_size = 25
+# рисование бонуса
+def draw_bonus(canvas, bonus_pos):
+    x, y = bonus_pos
+    canvas.create_oval(x, y, x + SNAKE_SIZE, y + SNAKE_SIZE, fill=BONUS_COLOR)
 
+# проверка столкновения со стеной
+def check_wall_collision(coords):
+    x, y = coords[-1]
+    if x < 0 or x >= CANVAS_WIDTH or y < 0 or y >= CANVAS_HEIGHT:
+        return True
+    else:
+        return False
 
-def get_speed(s):
-    global speed_sneak
-    speed_sneak = s
-def create_window():
-    global canvas, tk, text
-    tk = Tk()
-    tk.title("Игра Змейка на Python")
+# проверка столкновения с хвостом
+def check_tail_collision(coords):
+    head = coords[-1]
+    tail = coords[:-1]
+    if head in tail:
+        return True
+    else:
+        return False
 
-    canvas = Canvas(tk, width=game_width, height=game_height, bd=0, highlightthickness=0)
+# обработка нажатий клавиш
+def handle_key_press(event):
+    global direction
+    global bonus_active
+    if event.keysym in {'Up', 'Down', 'Left', 'Right'}:
+        if event.keysym == 'Up' and direction != 'Down':
+            direction = 'Up'
+        elif event.keysym == 'Down' and direction != 'Up':
+            direction = 'Down'
+        elif event.keysym == 'Left' and direction != 'Right':
+            direction = 'Left'
+        elif event.keysym == 'Right' and direction != 'Left':
+            direction = 'Right'
+    elif event.keysym == 'p':
+        start_game()
+    elif event.keysym == 'e':
+        root.destroy()
 
-    for i in range(presents_size):
-        x = random.randrange(virtual_game_x)
-        y = random.randrange(virtual_game_y)
-        id1 = canvas.create_oval(x * snake_item, y * snake_item, x * snake_item + snake_item, y * snake_item + snake_item,
-                                 fill=present_color2)
-        id2 = canvas.create_oval(x * snake_item + 2, y * snake_item + 2, x * snake_item + snake_item - 2,
-                                 y * snake_item + snake_item - 2, fill=present_color1)
-        presents_list.append([x, y, id1, id2])
-
-    canvas.bind_all("<KeyPress-Left>", snake_move)
-    canvas.bind_all("<KeyPress-Right>", snake_move)
-    canvas.bind_all("<KeyPress-Up>", snake_move)
-    canvas.bind_all("<KeyPress-Down>", snake_move)
-
-    canvas.pack()
-
-    text = canvas.create_text(50, 50, text=sneak_score, font="Arial 20", fill="black")
-
-# Прорисовка тела змейки
-def snake_paint_item(canvas, x, y):
-    global snake_list
-    id1 = canvas.create_rectangle(x * snake_item, y * snake_item, x * snake_item + snake_item,
-                                  y * snake_item + snake_item, fill=snake_color2)
-    id2 = canvas.create_rectangle(x * snake_item + 2, y * snake_item + 2, x * snake_item + snake_item - 2,
-                                  y * snake_item + snake_item - 2, fill=snake_color1)
-    snake_list.append([x, y, id1, id2])
-
-
-# Съедание яблока змейкой
-def check_can_we_delete_snake_item():
-    if len(snake_list) >= snake_size:
-        temp_item = snake_list.pop(0)
-        canvas.delete(temp_item[2])
-        canvas.delete(temp_item[3])
-
-# Проверка яблока
-def check_if_we_found_present():
-    global snake_size, sneak_score, text
-    for i in range(len(presents_list)):
-        if presents_list[i][0] == snake_x and presents_list[i][1] == snake_y:
-            snake_size = snake_size + 1
-            canvas.delete(presents_list[i][2])
-            canvas.delete(presents_list[i][3])
-
-# Движение змейки
-def snake_move(event):
-    global snake_x
-    global snake_y
-    global snake_x_nav
-    global snake_y_nav
-
-    if event.keysym == "Up":
-        snake_x_nav = 0
-        snake_y_nav = -1
-        check_can_we_delete_snake_item()
-    elif event.keysym == "Down":
-        snake_x_nav = 0
-        snake_y_nav = 1
-        check_can_we_delete_snake_item()
-    elif event.keysym == "Left":
-        snake_x_nav = -1
-        snake_y_nav = 0
-        check_can_we_delete_snake_item()
-    elif event.keysym == "Right":
-        snake_x_nav = 1
-        snake_y_nav = 0
-        check_can_we_delete_snake_item()
-    snake_x = snake_x + snake_x_nav
-    snake_y = snake_y + snake_y_nav
-    snake_paint_item(canvas, snake_x, snake_y)
-    check_if_we_found_present()
-
-
-# Выход из цикла
-def game_over():
-    global Game_Running
-    Game_Running = False
-
-
-# Выход за границы
-def check_if_borders():
-    if snake_x > virtual_game_x or snake_x < 0 or snake_y > virtual_game_y or snake_y < 0:
-        canvas.create_text(game_width / 2, game_height / 2,
-                           text="GAME OVER!",
-                           font="Arial 20",
-                           fill="#ff0000")
+# обновление игрового поля
+def update():
+    global snake_coords
+    global food_pos
+    global bonus_active
+    global bonus_pos
+    global score
+    score_var.set(f'Score: {score}')
+    # движение змеи
+    new_head = get_new_position(snake_coords, direction)
+    snake_coords.append(new_head)
+    # проверка столкновения со стеной/хвостом
+    if check_wall_collision(snake_coords) or check_tail_collision(snake_coords):
         game_over()
+        return
+    # проверка наличия еды
+    if snake_coords[-1] == food_pos:
+        score += 1
+        if score % 5 == 0:
+            bonus_active = True
+            bonus_pos = get_random_position()
+        else:
+            bonus_active = False
+        food_pos = get_random_position()
+    else:
+        snake_coords.pop(0)
+    # проверка наличия бонуса
+    if bonus_active and snake_coords[-1] == bonus_pos:
+        bonus_active = False
+        snake_coords = snake_coords[:-1]
+    # обновление холста
+    canvas.delete('all')
+    canvas.create_rectangle(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT, fill=BACKGROUND_COLOR)
+    draw_snake(canvas, snake_coords)
+    draw_food(canvas, food_pos)
+    if bonus_active:
+        draw_bonus(canvas, bonus_pos)
+    root.after(DELAY, update)
 
-# Столкновение со своим телом
-def check_we_touch_self(f_x, f_y):
-    global Game_Running
-    if not (snake_x_nav == 0 and snake_y_nav == 0):
-        for i in range(len(snake_list)):
-            if snake_list[i][0] == f_x and snake_list[i][1] == f_y:
-                canvas.create_text(game_width / 2, game_height / 2,
-                                   text="GAME OVER!",
-                                   font="Arial 20",
-                                   fill="#ff0000")
-                Game_Running = False
+# начало игры
+def start_game():
+    global snake_coords
+    global food_pos
+    global bonus_active
+    global bonus_pos
+    global score
+    score = 0
+    score_var.set(f'Score: {score}')
+    snake_coords = [(CANVAS_WIDTH // 2, CANVAS_HEIGHT // 2)]
+    food_pos = get_random_position()
+    bonus_active = False
+    bonus_pos = None
+    update()
 
-# Добавление очков
-def score():
-    global snake_size, sneak_score, text
-    for i in range(len(presents_list)):
-        if presents_list[i][0] == snake_x and presents_list[i][1] == snake_y:
-            sneak_score += 1
-            canvas.delete(text)
-            text = canvas.create_text(50, 50, text=sneak_score, font="Arial 20", fill="black")
+# конец игры
+def game_over():
+    canvas.create_text(CANVAS_WIDTH // 2, CANVAS_HEIGHT // 2, text='Game over', font=('Arial', 24), fill=TEXT_COLOR)
+    canvas.create_text(CANVAS_WIDTH // 2, CANVAS_HEIGHT // 2 + 30, text='Press P to play again or E to exit', font=('Arial', 16), fill=TEXT_COLOR)
 
-def restart():
-    global Game_Running, sneak_score, snake_x, snake_y, snake_x_nav, snake_y_nav, presents_list
-    root.destroy()
-    Game_Running = True
-    sneak_score = 0
-    snake_x = virtual_game_x // 2
-    snake_y = virtual_game_y // 2
-    snake_x_nav = 0
-    snake_y_nav = 0
-    presents_list = []
-    main()
+# создание окна
+root = Tk()
+root.title('Snake')
+root.resizable(0, 0)
 
-def main():
-    global snake_x, snake_y, root
+# создание холста
+canvas = Canvas(root, width=CANVAS_WIDTH, height=CANVAS_HEIGHT, bg=BACKGROUND_COLOR)
+canvas.pack()
 
-    create_window()
+# создание строки счёта
+score = 0
+score_var = StringVar()
+score_label = Label(root, textvariable=score_var, font=('Arial', 16), fg=TEXT_COLOR, bg=BACKGROUND_COLOR)
+score_label.pack()
 
-    while Game_Running:
-        check_can_we_delete_snake_item()
-        check_if_we_found_present()
-        score()
-        check_if_borders()
-        check_we_touch_self(snake_x + snake_x_nav, snake_y + snake_y_nav)
-        snake_x = snake_x + snake_x_nav
-        snake_y = snake_y + snake_y_nav
-        snake_paint_item(canvas, snake_x, snake_y)
-        tk.update()
-        time.sleep(speed_sneak)
+# обработка нажатий клавиш
+direction = 'Right'
+root.bind('<KeyPress>', handle_key_press)
 
-    tk.destroy()
+# запуск игры
+start_game()
 
-    root = Tk()
-    root.title("Конец")
-
-    Label(font="Arial 20", text="Ваш счёт: " + str(sneak_score), width=20, height=3).pack()
-    but1 = Button(text="End", width=20, command=root.destroy)
-    but2 = Button(text="Restart", width=20, command=restart)
-    but1.pack()
-    but2.pack()
-    root.mainloop()
-
-
-
-main()
+root.mainloop()
